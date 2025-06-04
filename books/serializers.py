13fd40932edf7c03ba6904da77426588ae588d2b
DenishaPatel <pateldenisha76@gmail.com>
2025-06-04 13:51:07 +0530

    CHANGE IN SERIALIZER

--- books/serializers.py
+++ books/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    # SERIALIZER FOR BOOK TABLE
     class Meta:
         model = Book
         fields = '__all__'
